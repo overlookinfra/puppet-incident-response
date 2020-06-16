8f87e721f5f70a81d441f5fbedcdc15943c71db3
Mikker Gimenez-Peterson <mikker+05250246@puppet.com>
2020-06-16 16:12:12 -0700

    Initial commit for the puppet django containern for the monzo response bot app

---
+++ Dockerfile
@@ -0,0 +1,21 @@
+FROM python:3.7-buster
+
+RUN apt-get update && apt-get install nginx vim -y --no-install-recommends
+COPY config/nginx.default /etc/nginx/sites-available/default
+RUN ln -sf /dev/stdout /var/log/nginx/access.log \
+    && ln -sf /dev/stderr /var/log/nginx/error.log
+
+RUN mkdir -p /opt/app
+RUN mkdir -p /opt/app/pip_cache
+RUN mkdir -p /opt/app/martor_demo
+COPY requirements.txt start-server.sh /opt/app/
+
+COPY . /opt/app/
+WORKDIR /opt/app
+RUN pip install -r requirements.txt
+RUN chown -R www-data:www-data /opt/app
+
+EXPOSE 8020
+STOPSIGNAL SIGTERM
+CMD ["/opt/app/start-server.sh"]
+
---
+++ puppetincidentresponse/base.py
@@ -0,0 +1,22 @@
+import os
+
+from django.core.exceptions import ImproperlyConfigured
+from response.slack.client import SlackClient
+
+def get_env_var(setting, warn_only=False):
+    value = os.getenv(setting, None)
+
+    if not value:
+        error_msg = f"ImproperlyConfigured: Set {setting} environment variable"
+        if warn_only:
+            logger.warn(error_msg)
+        else:
+            raise ImproperlyConfigured(error_msg)
+    else:
+        value = value.replace('"', "")  # remove start/end quotes
+
+    return value
+
+SLACK_TOKEN = get_env_var("SLACK_TOKEN")
+SLACK_CLIENT = SlackClient(SLACK_TOKEN)
+
