5da789e61799809c5d4356b40aa8cef02a9f18bd
Mikker Gimenez-Peterson <mikker+05250246@puppet.com>
2020-06-18 14:01:09 -0700

    Add production DATABASE_URL setting to settings.py

--- puppetincidentresponse/settings.py
+++ puppetincidentresponse/settings.py
@@ -79,12 +79,15 @@ WSGI_APPLICATION = 'puppetincidentresponse.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if os.getenv("DJANGO_ENV") == "prod":
+    DATABASES = {'default': env.db('DATABASE_URL')}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-}
 
 
 # Password validation
