d0049c8ecb491ccbdf35d972aceeb203653e2086
Mikker Gimenez-Peterson <mikker+05250246@puppet.com>
2020-07-01 14:51:18 -0700

    Adding collectstatic to puppetincidentresponse start

--- start-server.sh
+++ start-server.sh
@@ -4,6 +4,7 @@ if [ -n "$DJANGO_SUPERUSER_USERNAME" ] && [ -n "$DJANGO_SUPERUSER_PASSWORD" ] ; 
     (python manage.py createsuperuser --no-input)
 fi
 # Port 8010 is used her as it's the gunicorn port.  nginx listens on 8020 which is also the exposed port in the Dockerfile.
+python manage.py collectstatic
 (gunicorn puppetincidentresponse.wsgi --user www-data --bind 0.0.0.0:8010 --workers 3) &
 nginx -g "daemon off;"
 
